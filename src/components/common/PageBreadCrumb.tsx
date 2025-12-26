dfc9bc3e852f4daf8076de0ff6b4c3be1fc6f2b5
rakeshitfuturz <rakesh@itfuturz.com>
2025-12-26 14:57:08 +0530

    error fix 3

--- src/components/common/PageBreadCrumb.tsx
+++ src/components/common/PageBreadCrumb.tsx
@@ -1,7 +1,7 @@
 interface BreadcrumbProps {
   pageTitle: string;
 }
-
+import {LOCAL_STORAGE_KEYS} from "../../constants/localStorage";
 const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
   return (
     <div className="flex flex-wrap items-center justify-between gap-3 mb-6">
@@ -11,6 +11,13 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
       >
         {pageTitle}
       </h2>
+      <button 
+      className="bg-violet-700 rounded-md p-2 font-bold text-white border-2"
+      onClick={()=>{
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.VARIENTPRODUCT);
+        window.location.reload();
+      }}
+      >Clear Cache</button>
     </div>
   );
 };
